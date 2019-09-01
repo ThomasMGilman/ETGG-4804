a31096185c45b3c71786e0a68250b3955f1ccd4f
ThomasGilman <Thomas.Gilman@ymail.com>
2019-09-01 18:52:41 -0400

    Raytracer draws Triangles and Spheres

--- Raytracer Skeleton/Raytracer Skeleton/traceRay.h
+++ Raytracer Skeleton/Raytracer Skeleton/traceRay.h
@@ -7,65 +7,75 @@
 //ip : intersection point
 void traceSpheres(Scene& scene, vec3& s, vec3& v, vec3& ip, vec3& N, vec3& color, float& closestT, bool& inter)
 {
-	//std::cout << "sphere" << std::endl;
     unsigned ci=(unsigned)-1;
 
 	//go through the list of spheres in the scene 
 	//and determine if the viewer is intersecting the object
     for(unsigned i=0;i<scene.spheres.size();++i){
-		vec3 q = s - scene.spheres[i].c;
 
+		//q = rayStart - sphereCenter
+		//q * q - r^2 + 2(q * v)t + t^2(v * v) = 0
+		vec3 q = s - scene.spheres[i].c;
 		float radius = scene.spheres[i].r;
 		float A = dot(v, v);
 		float B = 2 * dot(q, v);
 		float C = dot(q, q) - (radius * radius);
+		float discriminant = (B * B) - (4 * A * C);
 
-		//use Quadratic Formula to check if a spheres t is closer and update closestT
-		float t1 = (-B + sqrt((B * B) - (4 * A * C))) / (2 * A);
-		float t2 = (-B - sqrt((B * B) - (4 * A * C))) / (2 * A);
+		if (discriminant >= 0)	//if the discriminant is less than zero, there is no intersection
+		{
+			float sqrtDiscrim = sqrt(discriminant);
+			float denom = 2 * A;
 
-		if (t1 < 0 && t2 >= 0)
-		{
-			if (closestT > t2)
+			//use Quadratic Formula to check if a spheres t's are closer and update closestT if they are
+			float t1 = (-B + sqrtDiscrim) / denom;
+			float t2 = (-B - sqrtDiscrim) / denom;
+
+			if (t1 < 0 && t2 >= 0)			//t2 is closer than ClosestT
 			{
-				closestT = t2;
-				ci = i;
+				if (closestT > t2)
+				{
+					closestT = t2;
+					ci = i;
+				}
 			}
-		}
-		else if (t1 >= 0 && t2 < 0)
-		{
-			if (closestT > t1)
+			else if (t1 >= 0 && t2 < 0)		//t1 is closer than ClosestT
 			{
-				closestT = t1;
-				ci = i;
+				if (closestT > t1)
+				{
+					closestT = t1;
+					ci = i;
+				}
 			}
-		}
-		else if (t1 >= 0 && t2 >= 0)
-		{
-			float minT = fminf(t1, t2);
-			if (closestT > minT)
+			else if (t1 >= 0 && t2 >= 0)	//one of the t's is closer than ClosestT
 			{
-				closestT = minT;
-				ci = i;
+				float minT = fminf(t1, t2);
+				if (closestT > minT)
+				{
+					closestT = minT;
+					ci = i;
+				}
 			}
 		}
     }
     
     if( ci == (unsigned)-1 ){
-        inter=false;
+		if (!inter)			//already true for another object
+			inter = false;
         return;
     }
-    ip = s + closestT * v;
-    N = normalize(ip - scene.spheres[ci].c);
-    color = scene.spheres[ci].color;
-    inter=true;
+	else
+	{
+		ip = s + closestT * v;						//intersection point
+		N = normalize(ip - scene.spheres[ci].c);	//Normal at intersection point
+		color = scene.spheres[ci].color;
+		inter = true;
+	}
 }
 
-//work on this too
 void traceTriangles(Scene& scene, vec3& s, vec3& v, vec3& ip, vec3& N, 
             vec3& color, float& closestT, bool& inter)
 {
-	//std::cout << "triangle" << std::endl;
 	unsigned ti = (unsigned)-1; //triangle intersection
 	Mesh *cm = nullptr;			//closest mesh to draw pointer
 
@@ -74,28 +84,37 @@ void traceTriangles(Scene& scene, vec3& s, vec3& v, vec3& ip, vec3& N,
 		
         for(unsigned i=0;i<M.triangles.size();++i){
             Triangle& T = M.triangles[i];
-			float denominator = dot(T.N, v);
+			vec3 p = T.p[0];
+			vec3 q = T.p[1];
+			vec3 r = T.p[2];
+
+			vec3 N = cross((q-r), (r-p));	//Triangles Normal
+			float D = -dot(N, p);			//Distance of plane from origin
+			float denominator = dot(N, v);
 			
 			//does the ray intersect the triangle
+			//No intersection if denominator is zero
 			if (denominator != 0)				
 			{
-				float t = (-(T.D + (dot(T.N, s)))) / denominator;
+				float t = -(D + dot(N, s)) / denominator;
 				if (t >= 0)
 				{
 					//std::cout << "got intersection: " << t << std::endl;
 					if (closestT > t)
 					{
-						//std::cout << "got closer intersection: " << t <<std::endl;
+						vec3 currentIP = s + (t*v); //intersection point
+
 						float sumQi = 0;
 						for (int i = 0; i < T.e->len(); i++)	//make sure ip is intersecting the triangle
 						{
-							vec3 qi = cross(T.e[i], (ip - T.p[i]));
+							vec3 qi = cross(T.e[i], (currentIP - T.p[i]));
 							sumQi += qi.magnitude();
 						}
 
 						float A = sumQi * T.oneOverTwiceArea;
 						if (A <= 1.001)
 						{
+							ip = currentIP;
 							closestT = t;
 							ti = i;
 							cm = &M;
@@ -107,9 +126,8 @@ void traceTriangles(Scene& scene, vec3& s, vec3& v, vec3& ip, vec3& N,
     }
 
 	if ( cm == nullptr || ti == (unsigned)-1) {	//no intersection
-
-		//std::cout << "no intersection, or mesh is null Mesh: " << std::endl;
-		inter = false;
+		if(!inter)			//already true for another object
+			inter = false;
 		return;
 	}
 	else
