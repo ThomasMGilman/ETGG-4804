f26907948509676427ee95ed6c58059dec827e91
ThomasMGilman <Thomas.Gilman@ymail.com>
2019-09-17 17:14:34 -0400

    start preprocessing assignment, collect data, optimize in next lab

---
+++ Lab 1/Raytracer Skeleton/Raytracer Skeleton/traceRay.h
@@ -0,0 +1,153 @@
+//Thomas Gilman
+//Jim Hudson
+//ETGG 4804_1
+//Optomization Techniques
+//3rd September, 2019
+#pragma once
+
+//s : ray start
+//v : ray direction
+//N : normal
+//ip : intersection point
+void traceSpheres(Scene& scene, vec3& s, vec3& v, vec3& ip, vec3& N, vec3& color, float& closestT, bool& inter)
+{
+    unsigned ci=(unsigned)-1;
+
+	//go through the list of spheres in the scene 
+	//and determine if the viewer is intersecting the object
+    for(unsigned i=0;i<scene.spheres.size();++i){
+
+		//q = rayStart - sphereCenter
+		//q * q - r^2 + 2(q * v)t + t^2(v * v) = 0
+		vec3 q = s - scene.spheres[i].c;
+		float radius = scene.spheres[i].r;
+		float A = dot(v, v);
+		float B = 2 * dot(q, v);
+		float C = dot(q, q) - (radius * radius);
+		float discriminant = (B * B) - (4 * A * C);
+
+		if (discriminant >= 0)	//if the discriminant is less than zero, there is no intersection
+		{
+			float sqrtDiscrim = sqrt(discriminant);
+			float denom = 2 * A;
+
+			//use Quadratic Formula to check if a spheres t's are closer and update closestT if they are
+			float t1 = (-B + sqrtDiscrim) / denom;
+			float t2 = (-B - sqrtDiscrim) / denom;
+
+			if (t1 < 0 && t2 >= 0)			//t2 is closer than ClosestT
+			{
+				if (closestT > t2)
+				{
+					closestT = t2;
+					ci = i;
+				}
+			}
+			else if (t1 >= 0 && t2 < 0)		//t1 is closer than ClosestT
+			{
+				if (closestT > t1)
+				{
+					closestT = t1;
+					ci = i;
+				}
+			}
+			else if (t1 >= 0 && t2 >= 0)	//one of the t's is closer than ClosestT
+			{
+				float minT = fminf(t1, t2);
+				if (closestT > minT)
+				{
+					closestT = minT;
+					ci = i;
+				}
+			}
+		}
+    }
+    
+    if( ci == (unsigned)-1 ){
+		if (!inter)			//already true for another object
+			inter = false;
+        return;
+    }
+	else
+	{
+		ip = s + closestT * v;						//intersection point
+		N = normalize(ip - scene.spheres[ci].c);	//Normal at intersection point
+		color = scene.spheres[ci].color;
+		inter = true;
+	}
+}
+
+void traceTriangles(Scene& scene, vec3& s, vec3& v, vec3& ip, vec3& N, 
+            vec3& color, float& closestT, bool& inter)
+{
+	unsigned ti = (unsigned)-1; //triangle intersection
+	Mesh *cm = nullptr;			//closest mesh to draw pointer
+
+	//check meshes for closest triangle
+    for(auto& M : scene.meshes ){
+		
+        for(unsigned i=0;i<M.triangles.size();++i){
+            Triangle& T = M.triangles[i];
+			vec3 p = T.p[0];
+			vec3 q = T.p[1];
+			vec3 r = T.p[2];
+
+			vec3 N = cross((q-r), (r-p));	//Triangles Normal
+			float D = -dot(N, p);			//Distance of plane from origin
+			float denominator = dot(N, v);
+			
+			//does the ray intersect the triangle
+			//No intersection if denominator is zero
+			if (denominator != 0)				
+			{
+				float t = -(D + dot(N, s)) / denominator;
+				if (t >= 0)
+				{
+					//std::cout << "got intersection: " << t << std::endl;
+					if (closestT > t)
+					{
+						vec3 currentIP = s + (t*v); //intersection point
+
+						float sumQi = 0;
+						for (int i = 0; i < T.e->len(); i++)	//make sure ip is intersecting the triangle
+						{
+							vec3 qi = cross(T.e[i], (currentIP - T.p[i]));
+							sumQi += qi.magnitude();
+						}
+
+						float A = sumQi * T.oneOverTwiceArea;
+						if (A <= 1.001)
+						{
+							ip = currentIP;
+							closestT = t;
+							ti = i;
+							cm = &M;
+						}
+					}
+				}
+			}
+        }
+    }
+
+	if ( cm == nullptr || ti == (unsigned)-1) {	//no intersection
+		if(!inter)			//already true for another object
+			inter = false;
+		return;
+	}
+	else
+	{
+		Triangle& T = cm->triangles[ti];		//get triangle to draw from closest mesh
+		N = T.N;
+		color = cm->color;
+		inter = true;
+	}
+}
+
+bool traceRay(Scene& scene, vec3& s, vec3& v, vec3& ip, vec3& N, vec3& color)
+{
+    float closestT = 1E99;
+    bool inter;
+    traceSpheres(scene,s,v,ip,N,color,closestT,inter);
+    traceTriangles(scene,s,v,ip,N,color,closestT,inter);
+    return inter;
+}
---
+++ Lab 2/Profiling_Lab/Profiling_Lab/floydsteinberg.cpp
@@ -0,0 +1,204 @@
+
+// Background info on FS dithering:
+//  http://www.tannerhelland.com/4660/dithering-eleven-algorithms-source-code/
+//  https://www.cyotek.com/blog/an-introduction-to-dithering-images
+
+#include <chrono>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <fstream>
+#include "math3d.h"
+#include "Image.h"
+
+Image computeDitheredNbitColor(Image img, int shiftCount)
+{
+    std::vector<vec3> error1(img.width());
+    std::vector<vec3> error2(img.width());
+    
+    int divisor = 1<<shiftCount;
+    
+    for(unsigned y=0;y<img.height();++y){
+        error1=error2;
+        for(unsigned i=0;i<error2.size();++i)
+            error2[i]=vec3(0,0,0);
+        for(unsigned x=0;x<img.width();++x){
+            uint8_t r,g,b,a;
+            img.getPixel(x,y,r,g,b,a);
+            ivec3 want = ivec3(r,g,b);
+            ivec3 error;
+            ivec3 out;
+            for(int i=0;i<3;++i){
+                want[i] += error1[x][i];
+                
+                out[i] = (want[i]/divisor) * divisor;
+                error[i] = want[i]-out[i];
+                
+                if( x < img.width()-1 ){
+                    error1[x+1][i] = 7*error[i]/16;
+                    error2[x+1][i] = 1*error[i]/16;
+                }
+                if( x > 0 )
+                    error2[x-1][i] = 3*error[i]/16;
+                error2[x][i] = 5*error[i]/16;
+            }
+            img.setPixel(x,y,out[0],out[1],out[2]);
+        }
+    }
+    return img;
+}
+Image computeDithered1BitColor(Image img){
+    std::vector<vec3> error1(img.width());
+    std::vector<vec3> error2(img.width());
+    
+    for(unsigned y=0;y<img.height();++y){
+        error1=error2;
+        for(unsigned i=0;i<error2.size();++i)
+            error2[i]=vec3(0,0,0);
+        for(unsigned x=0;x<img.width();++x){
+            uint8_t r,g,b,a;
+            img.getPixel(x,y,r,g,b,a);
+            vec3 want = vec3(r,g,b);
+            vec3 error;
+            vec3 out;
+            for(int i=0;i<3;++i){
+                want[i] += error1[x][i];
+                if( want[i] < 128 ){
+                    out[i] = 0;
+                    error[i] = want[i];
+                } else {
+                    out[i] = 255;
+                    error[i] = -(255-want[i]);
+                }
+                if( x < img.width()-1 ){
+                    error1[x+1][i] = 7*error[i]/16;
+                    error2[x+1][i] = 1*error[i]/16;
+                }
+                if( x > 0 )
+                    error2[x-1][i] = 3*error[i]/16;
+                error2[x][i] = 5*error[i]/16;
+            }
+            img.setPixel(x,y,out[0],out[1],out[2]);
+        }
+    }
+    return img;
+}
+
+
+Image computeGreyscale(Image img){
+    //convert to greyscale
+    for(unsigned y=0;y<img.height();++y){
+        for(unsigned x=0;x<img.width();++x){
+            uint8_t r,g,b,a;
+            img.getPixel(x,y,r,g,b,a);
+            int total = (int)(0.299*r + 0.587*g + 0.114*b);
+            if( total > 255 )
+                total = 255;
+            r = (uint8_t) total;
+            img.setPixel(x,y,r,r,r,255);
+        }
+    }
+    return img;
+}
+Image computeDitheredBilevel(Image img){
+    //floyd steinberg error diffusion
+    std::vector<int> error1(img.width());
+    std::vector<int> error2(img.width());
+    
+    for(unsigned y=0;y<img.height();++y){
+        error1=error2;
+        for(unsigned i=0;i<error2.size();++i)
+            error2[i]=0;
+        for(unsigned x=0;x<img.width();++x){
+            uint8_t r,g,b,a;
+            img.getPixel(x,y,r,g,b,a);
+            int want = r+g+b;
+            want /= 3;
+            want += error1[x];
+            int error;
+            if( want < 128 ){
+                //closer to black than white, so set output to black
+                img.setPixel(x,y,0,0,0,255);
+                error = want;
+            } else {
+                img.setPixel(x,y,255,255,255,255);
+                error = -(255-want);
+            }
+            if( x < img.width()-1 ){
+                error1[x+1] = 7*error/16;
+                error2[x+1] = 1*error/16;
+            }
+            if( x > 0 )
+                error2[x-1] = 3*error/16;
+            error2[x] = 5*error/16;
+        }
+    }
+    return img;
+}    
+
+int countColors(std::string filename){
+    std::vector<ivec3> M;
+    int unique=0;
+    Image img(filename);
+    for(unsigned y=0;y<img.height();++y){
+        for(unsigned x=0;x<img.width();++x){
+            uint8_t r,g,b,a;
+            img.getPixel(x,y,r,g,b,a);
+            ivec3 v(r,g,b);
+            if( find(M.begin(),M.end(),v) == M.end() ){
+                M.push_back(v);
+                unique++;
+            }
+        }
+    }
+    return unique;
+}
+
+int main(int argc, char* argv[])
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    std::vector<std::string> outputs;
+    
+    Image img1(argv[1]);
+    std::string fname;
+    
+    auto img = computeDitheredNbitColor(img1,4);
+    fname = "out-dithered-color-16.png";
+    img.writePng(fname);
+    outputs.push_back(fname);
+
+    img = computeDitheredNbitColor(img1,3);
+    fname = "out-dithered-color-32.png";
+    img.writePng(fname);
+    outputs.push_back(fname);
+    
+    img = computeDithered1BitColor(img1);
+    fname = "out-dithered-color-1.png";
+    img.writePng(fname);
+    outputs.push_back(fname);
+     
+    img = computeGreyscale(img1);
+    fname = "out-greyscale.png";
+    img.writePng(fname);
+    outputs.push_back(fname); 
+    
+    img = computeDitheredBilevel(img1);
+    fname = "out-dithered-bilevel.png";
+    img.writePng(fname);
+    outputs.push_back(fname);
+
+    std::ofstream countfile("out-colorcount.txt");
+    for(unsigned i=0;i<outputs.size();++i){
+        int numColors = countColors(outputs[i]);
+        countfile << outputs[i] << ": " << numColors << " unique colors\n";
+    }
+    countfile.close();
+    
+    auto end = std::chrono::high_resolution_clock::now();
+    auto delta = std::chrono::duration<double>(end-start).count();
+    std::cout << delta << " seconds\n";
+
+    return 0;
+}
+
